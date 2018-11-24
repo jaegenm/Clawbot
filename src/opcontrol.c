b31c896d6315698e953f48d20094d64c99459ca4
Jaegen Milley <45248570+jaegenm@users.noreply.github.com>
2018-11-24 15:48:20 -0800

    Fixed Bug: turn and power were reading the same value

--- src/opcontrol.c
+++ src/opcontrol.c
@@ -32,7 +32,7 @@ void operatorControl() {
 	int turn;
 	while (1) {
 		power = joystickGetAnalog(1, 2); // vertical axis on left joystick
-		turn = joystickGetAnalog(1, 2);  // horizontal axis on left joystick
+		turn = joystickGetAnalog(1, 1);  // horizontal axis on left joystick
 		chassisSet(power + turn, power - turn);
 		delay(20);
 	}
